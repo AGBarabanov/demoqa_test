74a5edbfc5f4219863d210aa0744bb3a6b583f04
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-08-21 18:21:35 +0300

    Add two tests

---
+++ src/test/java/JUnitExamples.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class JUnitExamples {
+
+    @Test
+    void firstTest() {
+        System.out.println("Here is firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Here is secondTest()");
+    }
+}
