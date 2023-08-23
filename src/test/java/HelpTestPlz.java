7b9af9a358655e583b525ca073456c6c1dc4a0f0
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-08-23 19:55:27 +0300

    created tests for firefox

--- src/test/java/HelpTestPlz.java
+++ src/test/java/HelpTestPlz.java
@@ -1,33 +1,29 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class HelpTestPlz {
 
-    static {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        Configuration.browserCapabilities = capabilities;
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.33.0-win64\\geckodriver.exe");
+        Configuration.browser = "firefox";
     }
 
     @Test
     void fillFormTest() {
-        Selenide.open("https://demoqa.com/text-box");
+        open("https://demoqa.com/text-box");
 
         $("#userName").setValue("Anton Barabanov");
-        $("#userEmail").setValue("anton.barabanov@gmail.com");
+        $("#userEmail").setValue("test@gmail.com");
         $("#currentAddress").setValue("Saransk");
         $("#permanentAddress").setValue("Saransk-2");
         $("#submit").click();
 
         $("#output").shouldHave(text("Anton Barabanov"), text("anton.barabanov@gmail.com"),
-                text("Saransk"), text("Saransk-2"));
+                 text("Saransk"), text("Saransk-2"));
     }
 }
