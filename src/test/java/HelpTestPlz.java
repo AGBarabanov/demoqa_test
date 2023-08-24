c7948b1fb4cf0754e36abb338052a3877c200acd
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-08-24 22:01:04 +0300

    Created test for site testpages

--- src/test/java/HelpTestPlz.java
+++ src/test/java/HelpTestPlz.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HelpTestPlz {
@@ -11,19 +13,30 @@ public class HelpTestPlz {
     public static void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.33.0-win64\\geckodriver.exe");
         Configuration.browser = "firefox";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl="https://testpages.herokuapp.com";
     }
 
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
 
-        $("#userName").setValue("Anton Barabanov");
-        $("#userEmail").setValue("test@gmail.com");
-        $("#currentAddress").setValue("Saransk");
-        $("#permanentAddress").setValue("Saransk-2");
-        $("#submit").click();
+        open("/styled/validation/input-validation.html");
 
-        $("#output").shouldHave(text("Anton Barabanov"), text("anton.barabanov@gmail.com"),
-                 text("Saransk"), text("Saransk-2"));
+        /*
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        */
+
+        $("#firstname").setValue("Anton");
+        $("#surname").setValue("Barabanovqwqwq");
+        $("#age").setValue("63");
+        $("#notes").setValue("Oh, i'm sorry!");
+        $("#country").selectOptionByValue("Denmark");
+
+        //С помощью by обращаемся к атрибуту и ищем значение
+        $(by("type", "submit")).click();
+
+        //$("#output").shouldHave(text("Anton Barabanov"), text("anton.barabanov@gmail.com"),
+                // text("Saransk"), text("Saransk-2"));
     }
 }
